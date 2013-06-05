357c9b1ff4e51fa8bd19631c0edbadc2079b592c
cyranothedaft <cyranothedev@gmail.com>
2013-06-05 17:11:00 -0600

    start of lib coding

--- src/lithium.core.lib.net45/Class1.cs
+++ src/lithium.core.lib.net45/Class1.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using lithium.core.lib.net45.Data;
+
+
 
 namespace lithium.core.lib.net45
 {
     public class Class1
     {
+       public void test() {
+          var n = new Network();
+       }
     }
 }
