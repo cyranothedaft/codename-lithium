9af910429b7f6849a2a2dbdaa21aac5bb2c7fac7
cyranothedaft <cyranothedev@gmail.com>
2013-06-11 17:21:58 -0600

    setting up for SQL CE

--- src/lithium.core.lib.net45/Data/NetDbModel.Designer.cs
+++ src/lithium.core.lib.net45/Data/NetDbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\files\priv\personal\eat\proj\dev\_github\codename-lithium\src\lithium.core.lib.net45\Data\NetDbModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\files\personal\proj\dev\codename-lithium\src\lithium.core.lib.net45\Data\NetDbModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
